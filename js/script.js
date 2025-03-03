3f2a5fcec6bdcf7a2908703c93b0163ce8b0561b
Enetwarch <enetwarch@protonmail.com>
2025-03-03 16:53:43 +0800

    refactored listener config function

--- js/script.js
+++ js/script.js
@@ -1,59 +1,44 @@
-window.addEventListener("load", addInputListeners);
+window.addEventListener("load", addListeners);
 
-const eventConfig = {
-    "enterInput": [
-        ["1", [["click", "one"], ["keyup", "Numpad1"], ["keyup", "Digit1"]]],
-        ["2", [["click", "two"], ["keyup", "Numpad2"], ["keyup", "Digit2"]]],
-        ["3", [["click", "three"], ["keyup", "Numpad3"], ["keyup", "Digit3"]]],
-        ["4", [["click", "four"], ["keyup", "Numpad4"], ["keyup", "Digit4"]]],
-        ["5", [["click", "five"], ["keyup", "Numpad5"], ["keyup", "Digit5"]]],
-        ["6", [["click", "six"], ["keyup", "Numpad6"], ["keyup", "Digit6"]]],
-        ["7", [["click", "seven"], ["keyup", "Numpad7"], ["keyup", "Digit7"]]],
-        ["8", [["click", "eight"], ["keyup", "Numpad8"], ["keyup", "Digit8"]]],
-        ["9", [["click", "nine"], ["keyup", "Numpad9"], ["keyup", "Digit9"]]],
-        ["0", [["click", "zero"], ["keyup", "Numpad0"], ["keyup", "Digit0"]]],
-        [".", [["click", "decimal"], ["keyup", "NumpadDecimal"], ["keyup", "Period"]]],
-        ["+", [["click", "add"], ["keyup", "NumpadAdd"]]],
-        ["-", [["click", "subtract"], ["keyup", "NumpadSubtract"]]],
-        ["×", [["click", "multiply"], ["keyup", "NumpadMultiply"]]],
-        ["÷", [["click", "divide"], ["keyup", "NumpadDivide"]]]
-    ],
-    "displayOutput": [["click", "equals"], ["keyup", "Equal"], ["keyup", "NumpadEnter"]],
-    "clearEntry": [["click", "clearEntry"], ["keyup", "Backspace"]],
-    "allClear": [["click", "allClear"], ["keyup", "Delete"]]
-}
-
-function addInputListeners() {
-    addEnterInputListeners();
-    addListener(eventConfig.displayOutput, displayOutput);
-    addListener(eventConfig.clearEntry, clearEntry);
-    addListener(eventConfig.allClear, allClear);
-}
-
-function addEnterInputListeners() {
-    const enterInputConfig = eventConfig.enterInput;
-    enterInputConfig.forEach(config => {
-        const input = config[0];
+function addListeners() {
+    const listenerConfig = [
+        [() => enterInput("1"), [["click", "one"], ["keyup", "Numpad1"], ["keyup", "Digit1"]]],
+        [() => enterInput("2"), [["click", "two"], ["keyup", "Numpad2"], ["keyup", "Digit2"]]],
+        [() => enterInput("3"), [["click", "three"], ["keyup", "Numpad3"], ["keyup", "Digit3"]]],
+        [() => enterInput("4"), [["click", "four"], ["keyup", "Numpad4"], ["keyup", "Digit4"]]],
+        [() => enterInput("5"), [["click", "five"], ["keyup", "Numpad5"], ["keyup", "Digit5"]]],
+        [() => enterInput("6"), [["click", "six"], ["keyup", "Numpad6"], ["keyup", "Digit6"]]],
+        [() => enterInput("7"), [["click", "seven"], ["keyup", "Numpad7"], ["keyup", "Digit7"]]],
+        [() => enterInput("8"), [["click", "eight"], ["keyup", "Numpad8"], ["keyup", "Digit8"]]],
+        [() => enterInput("9"), [["click", "nine"], ["keyup", "Numpad9"], ["keyup", "Digit9"]]],
+        [() => enterInput("0"), [["click", "zero"], ["keyup", "Numpad0"], ["keyup", "Digit0"]]],
+        [() => enterInput("."), [["click", "decimal"], ["keyup", "NumpadDecimal"], ["keyup", "Period"]]],
+        [() => enterInput("+"), [["click", "add"], ["keyup", "NumpadAdd"]]],
+        [() => enterInput("-"), [["click", "subtract"], ["keyup", "NumpadSubtract"]]],
+        [() => enterInput("×"), [["click", "multiply"], ["keyup", "NumpadMultiply"]]],
+        [() => enterInput("÷"), [["click", "divide"], ["keyup", "NumpadDivide"]]],
+        [() => displayOutput(), [["click", "equals"], ["keyup", "Equal"], ["keyup", "NumpadEnter"]]],
+        [() => clearEntry(), [["click", "clearEntry"], ["keyup", "Backspace"]]],
+        [() => allClear(), [["click", "allClear"], ["keyup", "Delete"]]]
+    ];
+    listenerConfig.forEach(config => {
+        const func = config[0];
         const listeners = config[1];
-        addListener(listeners, () => enterInput(input));
+        listeners.forEach(listener => {
+            const eventType = listener[0];
+            if (eventType === "click") {
+                const id = listener[1];
+                const element = document.getElementById(id);
+                element.addEventListener(eventType, func);
+            }
+            if (eventType === "keyup") {
+                const eventCode = listener[1];
+                window.addEventListener(eventType, event => {
+                    if (event.code === eventCode) func();
+                });
+            }    
+        });
     });
-}
-
-function addListener(listeners, func) {
-    for (const listener of listeners) {
-        const eventType = listener[0];
-        if (eventType === "click") {
-            const id = listener[1];
-            const element = document.getElementById(id);
-            element.addEventListener(eventType, func);
-        }
-        if (eventType === "keyup") {
-            const eventCode = listener[1];
-            window.addEventListener(eventType, (event) => {
-                if (event.code === eventCode) func();
-            });
-        }
-    }
 }
 
 const output = document.getElementById("output");
