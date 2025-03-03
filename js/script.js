226d014b26547fa0b676d7da8ebcf30646142207
Enetwarch <enetwarch@protonmail.com>
2025-03-03 20:41:37 +0800

    added functionality with bugs

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
 window.addEventListener("load", addListeners);
 
+const listenerConfig = [
+    [() => enterNumberInput("1"), [["click", "one"], ["keyup", "Numpad1"], ["keyup", "Digit1"]]],
+    [() => enterNumberInput("2"), [["click", "two"], ["keyup", "Numpad2"], ["keyup", "Digit2"]]],
+    [() => enterNumberInput("3"), [["click", "three"], ["keyup", "Numpad3"], ["keyup", "Digit3"]]],
+    [() => enterNumberInput("4"), [["click", "four"], ["keyup", "Numpad4"], ["keyup", "Digit4"]]],
+    [() => enterNumberInput("5"), [["click", "five"], ["keyup", "Numpad5"], ["keyup", "Digit5"]]],
+    [() => enterNumberInput("6"), [["click", "six"], ["keyup", "Numpad6"], ["keyup", "Digit6"]]],
+    [() => enterNumberInput("7"), [["click", "seven"], ["keyup", "Numpad7"], ["keyup", "Digit7"]]],
+    [() => enterNumberInput("8"), [["click", "eight"], ["keyup", "Numpad8"], ["keyup", "Digit8"]]],
+    [() => enterNumberInput("9"), [["click", "nine"], ["keyup", "Numpad9"], ["keyup", "Digit9"]]],
+    [() => enterNumberInput("0"), [["click", "zero"], ["keyup", "Numpad0"], ["keyup", "Digit0"]]],
+    [() => enterNumberInput("."), [["click", "decimal"], ["keyup", "NumpadDecimal"], ["keyup", "Period"]]],
+    [() => enterOperationInput("+"), [["click", "add"], ["keyup", "NumpadAdd"]]],
+    [() => enterOperationInput("-"), [["click", "subtract"], ["keyup", "NumpadSubtract"]]],
+    [() => enterOperationInput("×"), [["click", "multiply"], ["keyup", "NumpadMultiply"]]],
+    [() => enterOperationInput("÷"), [["click", "divide"], ["keyup", "NumpadDivide"]]],
+    [() => displayOutput(), [["click", "equals"], ["keyup", "Equal"], ["keyup", "NumpadEnter"]]],
+    [() => clearEntry(), [["click", "clearEntry"], ["keyup", "Backspace"]]],
+    [() => allClear(), [["click", "allClear"], ["keyup", "Delete"]]]
+];
+
 function addListeners() {
-    const listenerConfig = [
-        [() => enterInput("1"), [["click", "one"], ["keyup", "Numpad1"], ["keyup", "Digit1"]]],
-        [() => enterInput("2"), [["click", "two"], ["keyup", "Numpad2"], ["keyup", "Digit2"]]],
-        [() => enterInput("3"), [["click", "three"], ["keyup", "Numpad3"], ["keyup", "Digit3"]]],
-        [() => enterInput("4"), [["click", "four"], ["keyup", "Numpad4"], ["keyup", "Digit4"]]],
-        [() => enterInput("5"), [["click", "five"], ["keyup", "Numpad5"], ["keyup", "Digit5"]]],
-        [() => enterInput("6"), [["click", "six"], ["keyup", "Numpad6"], ["keyup", "Digit6"]]],
-        [() => enterInput("7"), [["click", "seven"], ["keyup", "Numpad7"], ["keyup", "Digit7"]]],
-        [() => enterInput("8"), [["click", "eight"], ["keyup", "Numpad8"], ["keyup", "Digit8"]]],
-        [() => enterInput("9"), [["click", "nine"], ["keyup", "Numpad9"], ["keyup", "Digit9"]]],
-        [() => enterInput("0"), [["click", "zero"], ["keyup", "Numpad0"], ["keyup", "Digit0"]]],
-        [() => enterInput("."), [["click", "decimal"], ["keyup", "NumpadDecimal"], ["keyup", "Period"]]],
-        [() => enterInput("+"), [["click", "add"], ["keyup", "NumpadAdd"]]],
-        [() => enterInput("-"), [["click", "subtract"], ["keyup", "NumpadSubtract"]]],
-        [() => enterInput("×"), [["click", "multiply"], ["keyup", "NumpadMultiply"]]],
-        [() => enterInput("÷"), [["click", "divide"], ["keyup", "NumpadDivide"]]],
-        [() => displayOutput(), [["click", "equals"], ["keyup", "Equal"], ["keyup", "NumpadEnter"]]],
-        [() => clearEntry(), [["click", "clearEntry"], ["keyup", "Backspace"]]],
-        [() => allClear(), [["click", "allClear"], ["keyup", "Delete"]]]
-    ];
     listenerConfig.forEach(config => {
         const func = config[0];
         const listeners = config[1];
@@ -42,22 +43,74 @@ function addListeners() {
 }
 
 const output = document.getElementById("output");
+const operators = ["+", "-", "×", "÷"];
+const operations = {
+    "+": (x, y) => x + y,
+    "-": (x, y) => x - y,
+    "×": (x, y) => x * y,
+    "÷": (x, y) => x / y
+};
 
-function enterInput(input) {
+let x = undefined;
+let y = undefined;
+let operator = undefined;
+
+function enterNumberInput(input) {
+    output.innerText += input;
+}
+
+function enterOperationInput(input) {
+    const text = output.innerText;
+    const recentEntryChar = text.charAt(text.length - 1);
+    const recentEntryIsEmpty = recentEntryChar === "";
+    if (input === recentEntryChar) return;
+    if (input === "-") {
+        const forNegativeNumber = operators.includes(recentEntryChar);
+        if (recentEntryIsEmpty || forNegativeNumber) {
+            output.innerText += input;
+            return;
+        }
+    }
+    if (recentEntryIsEmpty) return;
+    if (operators.includes(recentEntryChar)) return;
+    if (x === undefined) {
+        x = parseInt(text);
+    } else {
+        displayOutput();
+    }
+    operator = input;
     output.innerText += input;
 }
 
 function displayOutput() {
-    console.log("Display output is under development");
+    if (operator !== undefined) {
+        if (y === undefined) {
+            const text = output.innerText;
+            const variables = text.split(operator);
+            const lastVariable = variables[variables.length - 1];
+            y = parseInt(lastVariable);
+        }
+        const operation = operations[operator];
+        const result = operation(x, y);
+        x = result;
+        y = operator = undefined;
+        output.innerText = `${result}`;
+    }
 }
 
 function clearEntry() {
     if (output.length !== 0) {
         const text = output.innerText;
-        output.innerText = text.substring(0, text.length - 1);
+        const recentEntryIndex = text.length -1;
+        const recentEntryChar = text.charAt(recentEntryIndex);
+        if (recentEntryChar === operator) {
+            operator = undefined;
+        }
+        output.innerText = text.substring(0, recentEntryIndex);
     }
 }
 
 function allClear() {
+    x = y = operator = undefined;
     output.innerText = "";
 }
