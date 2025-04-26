0c2cda642a2dd41d37db25758d86b5bed725672b
Enetwarch <enetwarch@protonmail.com>
2025-04-26 08:03:31 +0800

    added more logic and testing

--- src/utils/logic.test.ts
+++ src/utils/logic.test.ts
@@ -2,9 +2,10 @@ import {
   type Digit,
   type Input,
   type Operation,
-  type Output,
   type Sign,
+  controlOutput,
   digits,
+  getLastCharacter,
   getLastTerm,
   inputs,
   isDigit,
@@ -12,7 +13,9 @@ import {
   isSign,
   operations,
   signs,
+  stringifyOutput,
   validateDecimalInput,
+  validateOperationInput,
 } from "@/utils/logic";
 
 type testIsTypeFnArgs<Parent, Child extends Parent> = {
@@ -69,36 +72,31 @@ testIsTypeFn<Input, Sign>({
   parentTypes: inputs,
 });
 
-describe("getLastTerm", () => {
-  it("should return the same output back if there are no operations", () => {
-    const outputs: Output[] = [
-      ["1", "2", "3", ".", "4", "5", "6"],
-      ["negative", "6", "7", "8", "9", "1"],
-      ["negative"],
-    ];
+describe(getLastCharacter.name, () => {
+  it("should return undefined if the output is empty", () => {
+    expect(getLastCharacter([])).toBe(undefined);
+  });
 
-    for (const output of outputs) {
-      expect(getLastTerm(output)).toEqual(output);
-    }
+  it("should return the last element in the output array", () => {
+    expect(getLastCharacter(["2", "0"])).toBe("0");
+    expect(getLastCharacter(["3", "2", "1"])).toBe("1");
+  });
+
+  it("should return an expression if that is the last element in the array", () => {
+    expect(getLastCharacter(["negative"])).toBe("negative");
+    expect(getLastCharacter(["1", "times"])).toBe("times");
+  });
+});
+
+describe(getLastTerm.name, () => {
+  it("should return the same output back if there are no operations", () => {
+    expect(getLastTerm(["negative"])).toEqual(["negative"]);
+    expect(getLastTerm(["1", "2", ".", "3"])).toEqual(["1", "2", ".", "3"]);
   });
 
   it("should return the last term if there is an operation", () => {
-    const inputAndOutput: [Output, Output][] = [
-      [["1", "plus", "2"], ["2"]],
-      [
-        ["negative", "2", "minus", "4", "7"],
-        ["4", "7"],
-      ],
-      [
-        ["4", "7", "plus", "negative", "2"],
-        ["negative", "2"],
-      ],
-      [["1", "plus", "negative"], ["negative"]],
-    ];
-
-    for (const [input, output] of inputAndOutput) {
-      expect(getLastTerm(input)).toEqual(output);
-    }
+    expect(getLastTerm(["4", "7", "plus", "negative", "2"])).toEqual(["negative", "2"]);
+    expect(getLastTerm(["1", "plus", "negative"])).toEqual(["negative"]);
   });
 
   it("should return an empty array if the output is empty", () => {
@@ -106,44 +104,132 @@ describe("getLastTerm", () => {
   });
 
   it("should return an empty array if the last term is empty", () => {
-    const inputs: Output[] = [
-      ["1", "plus"],
-      ["6", "9", "4", "2", "0", "dividedBy"],
-    ];
-
-    for (const input of inputs) {
-      expect(getLastTerm(input)).toEqual([]);
-    }
+    expect(getLastTerm(["1", "plus"])).toEqual([]);
+    expect(getLastTerm(["6", "9", "4", "2", "0", "dividedBy"])).toEqual([]);
   });
 });
 
-describe("validateDecimalInput", () => {
+describe(validateDecimalInput.name, () => {
   it("should return false if the first input is a decimal", () => {
-    expect(validateDecimalInput([])).toBe(false);
+    expect(validateDecimalInput(".", [])).toBe(false);
   });
 
   it("should return false if the last character is an operation", () => {
-    expect(validateDecimalInput(["1", "plus"])).toBe(false);
-    expect(validateDecimalInput(["1", "2", "3", "dividedBy"])).toBe(false);
+    expect(validateDecimalInput(".", ["1", "plus"])).toBe(false);
+    expect(validateDecimalInput(".", ["1", "2", "3", "dividedBy"])).toBe(false);
   });
 
   it("should return false if the last character of the output is a sign", () => {
-    expect(validateDecimalInput(["negative"])).toBe(false);
-    expect(validateDecimalInput(["1", "plus", "negative"])).toBe(false);
+    expect(validateDecimalInput(".", ["negative"])).toBe(false);
+    expect(validateDecimalInput(".", ["1", "plus", "negative"])).toBe(false);
   });
 
   it("should return false if the last character of the output is already a decimal", () => {
-    expect(validateDecimalInput(["1", "."])).toBe(false);
-    expect(validateDecimalInput(["0", "1", "plus", "9", "."])).toBe(false);
+    expect(validateDecimalInput(".", ["1", "."])).toBe(false);
+    expect(validateDecimalInput(".", ["0", "1", "plus", "9", "."])).toBe(false);
   });
 
   it("should return false if there is already a decimal point in the last term of the output", () => {
-    expect(validateDecimalInput(["1", "2", ".", "3"])).toBe(false);
-    expect(validateDecimalInput(["1", "plus", "3", "1", ".", "5"])).toBe(false);
+    expect(validateDecimalInput(".", ["1", "2", ".", "3"])).toBe(false);
+    expect(validateDecimalInput(".", ["1", "plus", "3", "1", ".", "5"])).toBe(false);
   });
 
-  it("should return true if the last character of the output is a digit", () => {
-    expect(validateDecimalInput(["6", "9"])).toBe(true);
-    expect(validateDecimalInput(["1", "plus", "2"])).toBe(true);
+  it("should return true if the last character of the output is a digit and it passes every false", () => {
+    expect(validateDecimalInput(".", ["6", "9"])).toBe(true);
+    expect(validateDecimalInput(".", ["1", "plus", "2"])).toBe(true);
+  });
+});
+
+describe(validateOperationInput.name, () => {
+  it("should return false if the last character is a decimal", () => {
+    expect(validateOperationInput("plus", ["1", "."])).toBe(false);
+    expect(validateOperationInput("times", ["6", "9", "."])).toBe(false);
+  });
+
+  it("should return false if the last character is a sign", () => {
+    expect(validateOperationInput("minus", ["negative"])).toBe(false);
+  });
+
+  it("should return false if the last character is already an operation and the input is not a minus", () => {
+    expect(validateOperationInput("plus", ["1", "plus"])).toBe(false);
+    expect(validateOperationInput("dividedBy", ["negative", "1", "minus"])).toBe(false);
+  });
+
+  it("should return false if the input is a minus and the last character is already a minus", () => {
+    expect(validateOperationInput("minus", ["1", "minus"])).toBe(false);
+  });
+
+  it("should return false if the output is empty and the operation is not minus", () => {
+    const nonMinusOperations: Operation[] = ["plus", "times", "dividedBy"];
+    for (const operation of nonMinusOperations) {
+      expect(validateOperationInput(operation, [])).toBe(false);
+    }
+  });
+
+  it("should return true if the input is a minus and the last character is another operation", () => {
+    expect(validateOperationInput("minus", ["1", "plus"])).toBe(true);
+    expect(validateOperationInput("minus", ["3", "2", "1", "dividedBy"])).toBe(true);
+  });
+
+  it("should return true if the input is a minus and the output is empty", () => {
+    expect(validateOperationInput("minus", [])).toBe(true);
+  });
+
+  it("should return true if the last character is a digit", () => {
+    expect(validateOperationInput("plus", ["1"])).toBe(true);
+    expect(validateOperationInput("minus", ["1", "9"])).toBe(true);
+  });
+});
+
+describe(stringifyOutput.name, () => {
+  it("should convert an empty output to an empty string", () => {
+    expect(stringifyOutput([])).toBe("");
+  });
+
+  it(`should correctly convert "negative" to "-"`, () => {
+    expect(stringifyOutput(["negative"])).toBe("-");
+    expect(stringifyOutput(["negative", "1", "0"])).toBe("-10");
+  });
+
+  it(`should correctly convert "plus" to "+"`, () => {
+    expect(stringifyOutput(["1", "plus", "1"])).toBe("1+1");
+    expect(stringifyOutput(["2", "plus"])).toBe("2+");
+  });
+
+  it(`should correctly convert "minus" to "-"`, () => {
+    expect(stringifyOutput(["1", "minus", "1"])).toBe("1-1");
+    expect(stringifyOutput(["6", "9", "minus", "4", "2", "0"])).toBe("69-420");
+  });
+
+  it(`should correctly convert "times" to "×"`, () => {
+    expect(stringifyOutput(["6", "times", "9"])).toBe("6×9");
+    expect(stringifyOutput(["1", "times", "2", "7"])).toBe("1×27");
+  });
+
+  it(`should correctly convert "dividedBy" to "÷"`, () => {
+    expect(stringifyOutput(["6", "dividedBy", "9"])).toBe("6÷9");
+    expect(stringifyOutput(["1", "2", "dividedBy", "2", "4"])).toBe("12÷24");
+  });
+
+  it("should correctly display numbers", () => {
+    expect(stringifyOutput(["6", "9", "4", "2", "0"])).toBe("69420");
+    expect(stringifyOutput(["0"])).toBe("0");
+  });
+
+  it("should correctly display numbers with decimals", () => {
+    expect(stringifyOutput(["6", "9", ".", "4", "2", "0"])).toBe("69.420");
+    expect(stringifyOutput(["0", ".", "1"])).toBe("0.1");
+  });
+});
+
+describe(controlOutput.name, () => {
+  it(`should return an empty array with the "allClear" control`, () => {
+    expect(controlOutput("allClear", ["1", "2", "3"])).toEqual([]);
+    expect(controlOutput("allClear", ["6", "times", "9"])).toEqual([]);
+  });
+
+  it(`should return an output without the previous last character with the "clearEntry" control`, () => {
+    expect(controlOutput("clearEntry", ["1", "2", "3"])).toEqual(["1", "2"]);
+    expect(controlOutput("clearEntry", ["6", "times", "9"])).toEqual(["6", "times"]);
   });
 });
--- src/utils/logic.ts
+++ src/utils/logic.ts
@@ -9,14 +9,85 @@ export type Operation = (typeof operations)[number];
 export type Sign = (typeof signs)[number];
 export type Control = (typeof controls)[number];
 export type Input = (typeof inputs)[number];
+
+export const infinity = [["Infinity"], ["-Infinity"]] as const;
+export type Infinity = (typeof infinity)[number];
 export type Output = Input[];
 
-export function validateDecimalInput(output: Output): boolean {
+type OperationLiteral = "+" | "-" | "×" | "÷";
+type SignLiteral = "-";
+
+const operationMap: Record<Operation, OperationLiteral> = Object.freeze({
+  plus: "+",
+  minus: "-",
+  times: "×",
+  dividedBy: "÷",
+});
+
+const signMap: Record<Sign, SignLiteral> = Object.freeze({
+  negative: "-",
+});
+
+export function controlOutput(control: Control, output: Output): Output {
+  if (control === "allClear") return [];
+  if (control === "clearEntry") return output.slice(0, -1);
+
+  throw Error(`Invalid control and output: ${control}, ${output}`);
+}
+
+export function stringifyOutput(output: Output): string {
+  const outputStrings: string[] = [];
+
+  for (const character of output) {
+    const characterString: string = ((): string => {
+      if (isOperation(character)) return operationMap[character];
+      if (isSign(character)) return signMap[character];
+      if (isDigit(character)) return character;
+
+      throw Error(`Invalid character: ${character}`);
+    })();
+
+    outputStrings.push(characterString);
+  }
+
+  return outputStrings.join("");
+}
+
+export function validateInput(input: Input, output: Output): boolean {
+  if (isOperation(input)) return validateOperationInput(input, output);
+  if (isDigit(input)) return validateDigitInput(input, output);
+
+  throw Error(`Invalid input and output: ${input}, ${output}`);
+}
+
+export function validateDigitInput(digit: Digit, output: Output): boolean {
+  if (digit === ".") return validateDecimalInput(digit, output);
+  return true;
+}
+
+export function validateOperationInput(operation: Operation, output: Output): boolean {
+  const lastCharacter: Input = getLastCharacter(output);
+
+  if (lastCharacter === ".") return false;
+  if (isSign(lastCharacter)) return false;
+
+  if (output.length === 0 && operation !== "minus") return false;
+  if (isOperation(lastCharacter) && operation !== "minus") return false;
+  if (lastCharacter === "minus" && operation === "minus") return false;
+
+  if (output.length === 0 && operation === "minus") return true;
+  if (isOperation(lastCharacter) && operation === "minus") return true;
+  if (isDigit(lastCharacter)) return true;
+
+  throw Error(`Invalid operation and last character: ${operation}, ${lastCharacter}`);
+}
+
+export function validateDecimalInput(decimal: ".", output: Output): boolean {
   if (output.length < 1) return false;
 
   const lastTerm: Output = getLastTerm(output);
   for (const character of lastTerm) {
-    if (character === ".") return false;
+    if (character === decimal) return false;
   }
 
   const lastCharacter: Input = getLastCharacter(output);
